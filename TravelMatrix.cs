5bacb19d3f79353874983053f9f58e1969888876
someName <some@mailadres>
2014-11-23 11:11:22 +0100

    changed Matrx.cs

--- TravelMatrix.cs
+++ TravelMatrix.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.CSharp;
 
 public struct Route
 {
-    //simple tuple struct
-    // waar zat de point classe ook weer?
     public int X;
     public int Y;
+    public int Time;
+    public int Distance;
 
-    public Route(int x, int y)
+    public Route(int x, int y,int t, int d)
     {
         X = x; 
         Y = y;
+        Time = t;
+        Distance = d;
     }
 }
 
@@ -21,8 +24,7 @@ static public class TravelMatrix
 {
     //class to save the distanceMatrix in
 
-    static Dictionary<Route, int> Afstanden = new Dictionary<Route, int>();
-    static Dictionary<Route, int> Tijden = new Dictionary<Route, int>();
+    static Dictionary<Tuple<int, int>, Route> Routes = new Dictionary<Tuple<int, int>, Route>();
 
     static public void Init()
     {
@@ -45,21 +47,11 @@ static public class TravelMatrix
     private static void addRoute(int id1, int id2, int a, int t)
     {
         //save time and dist of this route
-        Route r = new Route(id1,id2);
-        Afstanden.Add(r, a);
-        Tijden.Add(r, t);
+        Routes.Add(new Tuple<int,int>(id1,id2),new Route(id1,id2,t,a));
     }
-    public static int GetTime(int loc1, int loc2)
-    {
-        //gets the traveling time between two locations
-        Route r = new Route(loc1, loc2);
-        return Tijden[r];
 
-    }
-    public static int GetDistance(int loc1, int loc2)
+    public static Route GetRoute(int loc1, int loc2)
     {
-        //gets the traveling time between two locations
-        Route r = new Route(loc1, loc2);
-        return Tijden[r];
-   }
+        return Routes[new Tuple<int, int>(loc1, loc2)];
+    }
 }
